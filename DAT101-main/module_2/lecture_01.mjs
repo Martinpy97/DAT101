f440dbef8d2ae415f40be22189dbb24f2923a3c9
Martin <martinho@uia.no>
2024-09-04 14:44:01 +0200

    Oppgave 2.1 test 1

--- DAT101-main/module_2/lecture_01.mjs
+++ DAT101-main/module_2/lecture_01.mjs
@@ -1,3 +1,9 @@
 import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
+
 printOut("Test");
+
+let v1 = 2;
+const v2 = 'Dette er en tekst';
+const v3 = 1.2455;
+const v4 = 12;
