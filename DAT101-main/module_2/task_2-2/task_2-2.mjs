d12f854de5f91a99b4460d426067876ca2f3fa53
Martin <martinho@uia.no>
2024-09-11 13:46:02 +0200

    task 2.2 øving 11.09

--- DAT101-main/module_2/task_2-2/task_2-2.mjs
+++ DAT101-main/module_2/task_2-2/task_2-2.mjs
@@ -4,22 +4,58 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+2 + 3 * 
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+const millInInch = 25.4;
+const millInMeters = 1000;
+const meters = 25.34;
+const answer = meters / millInMeters * millInInch;
+const exactAnswer = answer.toFixed(2);
+printOut('exact answer = ' + exactAnswer.toString());
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const HoursInDay = 24;
+const part3_answer =
+(3 * HoursInDay * minutesInHour) + 
+(12 * minutesInHour) + 
+14 + 
+(45 / secondsInMinute);
+
+printOut('part3_answer = ' + part3_answer.toString());
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const numberOfMinutes = 6322.52;
+let part4_calc = numberOfMinutes / (60 * 24);
+const part4_days = Math.floor(part4_calc);
+printOut("days = " + part4_days );
+
+part4_calc = part4_calc - part4_days;
+part4_calc = part4_calc * HoursInDay;
+const part4_Hours = Math.floor(part4_calc);
+printOut('Hours = ' + part4_Hours );
+
+part4_calc = part4_calc - part4_Hours;
+part4_calc = part4_calc * minutesInHour;
+const part4_Minutes = Math.floor(part4_calc);
+printOut('Minutes = '+ part4_Minutes);
+
+part4_calc = part4_calc - part4_Minutes;
+part4_calc = part4_calc * secondsInMinute;
+const part4_Seconds = Math.floor(part4_calc);
+printOut('Seconds = ' + part4_Seconds);
+
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
