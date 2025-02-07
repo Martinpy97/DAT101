8ace32b133f3186e9faab6adf2aab2ec1c564ac6
Martin <holte.martin@outlook.com>
2025-02-07 09:03:33 +0100

    update minesweeper

---
+++ DAT101-main/module_5/Minesweeper/Minesweeper.mjs
@@ -0,0 +1,95 @@
+"use strict";
+import lib2d from "../../common/libs/lib2d_v2.mjs";
+import libSprite from "../../common/libs/libSprite_v2.mjs";
+import { TGameBoard } from "./GameBoard.mjs";
+import { TTile } from "./Tile.mjs";
+
+//-----------------------------------------------------------------------------------------
+//----------- variables and object --------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+export const SpriteInfoList = {
+  Board: {
+    TopLeft: { x: 0, y: 0, width: 163, height: 133, count: 1 },
+    TopMiddle: { x: 163, y: 0, width: 134, height: 133, count: 1 },
+    TopRight: { x: 297, y: 0, width: 163, height: 133, count: 1 },
+    LeftMiddle: { x: 0, y: 133, width: 21, height: 243, count: 1 },
+    RightMiddle: { x: 439, y: 133, width: 21, height: 243, count: 1 },
+    BottomLeft: { x: 0, y: 377, width: 21, height: 21, count: 1 },
+    BottomMiddle: { x: 21, y: 377, width: 417, height: 21, count: 1 },
+    BottomRight: { x: 439, y: 377, width: 21, height: 21, count: 1 },
+  },
+  ButtonTile: { x: 0, y: 482, width: 50, height: 50, count: 8 },
+  ButtonSmiley: { x: 0, y: 532, width: 82, height: 82, count: 6 },
+  Numbers: { x: 0, y: 398, width: 46, height: 84, count: 10 },
+};
+
+const Difficulty = {
+  Level_1: { Tiles: { Row: 10, Col: 10 }, Mines: 4, caption: "Level 1" },
+  Level_2: { Tiles: { Row: 15, Col: 15 }, Mines: 20, caption: "Level 2" },
+  Level_3: { Tiles: { Row: 20, Col: 30 }, Mines: 99, caption: "Level 3" },
+};
+
+export let gameLevel = Difficulty.Level_1;
+
+const cvs = document.getElementById("cvs");
+const spcvs = new libSprite.TSpriteCanvas(cvs);
+
+const selectDifficulty = document.getElementById("selectDifficulty");
+
+
+export const gameProps = {
+  gameBoard: null,
+  tile: null,
+}
+//-----------------------------------------------------------------------------------------
+//----------- functions -------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+function loadGame() {
+  newGame();
+  drawGame();
+}
+
+//-----------------------------------------------------------------------------------------
+export function newGame() {
+  cvs.width = gameLevel.Tiles.Col * SpriteInfoList.ButtonTile.width + SpriteInfoList.Board.LeftMiddle.width + SpriteInfoList.Board.RightMiddle.width;
+  cvs.height = gameLevel.Tiles.Row * SpriteInfoList.ButtonTile.height + SpriteInfoList.Board.TopMiddle.height + SpriteInfoList.Board.BottomMiddle.height;
+  spcvs.updateBoundsRect();
+  gameProps.gameBoard = new TGameBoard(spcvs, SpriteInfoList.Board, new lib2d.TPoint(0, 0));
+  const pos = new lib2d.TPoint(20, 132);
+  gameProps.tile = new TTile(spcvs, SpriteInfoList.ButtonTile, pos);
+}
+
+
+function drawGame() {
+  spcvs.clearCanvas();
+  gameProps.gameBoard.draw();
+  gameProps.tile.draw();
+  requestAnimationFrame(drawGame);
+  
+}
+
+//-----------------------------------------------------------------------------------------
+//----------- Events ----------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+
+function selectDifficultyChange(e) {
+  switch (e.target.value) {
+    case "Level_1":
+      gameLevel = Difficulty.Level_1;
+      break;
+    case "Level_2":
+      gameLevel = Difficulty.Level_2;
+      break;
+    case "Level_3":
+      gameLevel = Difficulty.Level_3;
+      break;
+  }
+  newGame();
+}
+
+//-----------------------------------------------------------------------------------------
+//----------- Main Program ----------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------
+
+spcvs.loadSpriteSheet("./media/spriteSheet.png", loadGame);
+selectDifficulty.addEventListener("change", selectDifficultyChange);
---
+++ DAT101-main/module_5/Minesweeper/Tile.mjs
@@ -0,0 +1,15 @@
+"use strict";
+import libSprite from "../../common/libs/libSprite_v2.mjs";
+
+export class TTile extends libSprite.TSpriteButton{
+    
+    constructor(aSpriteCanvas, aSpriteInfo, aPos){
+        super(aSpriteCanvas, aSpriteInfo, aPos);
+        
+
+
+}
+
+
+}
+
